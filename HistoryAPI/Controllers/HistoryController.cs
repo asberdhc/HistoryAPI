beba3368e2fb39715024c7cef591493de3140a37
Alejandro <asber_dhc@hotmail.es>
2020-02-17 21:26:58 -0600

    Adding the full project

---
+++ HistoryAPI/Controllers/HistoryController.cs
@@ -0,0 +1,63 @@
+﻿using HistoryAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HistoryAPI.Controllers
+{
+    public class HistoryController : ApiController
+    {
+        YouTubeEntities _dbContext = new YouTubeEntities();
+
+        //GET api/history
+        [HttpGet]
+        public List<VideoWatchedDto> GetVideosWatched(int amount = 10)
+        {
+            var lastVIdeosWatched = _dbContext.History
+                                      .Where(h => h.IsEnabled == true)
+                                      .OrderByDescending(v => v.Id)
+                                      .Take(amount)
+                                      .Select(h => new VideoWatchedDto {
+                                          Id = h.Id,
+                                          VideoId = h.VideoId,
+                                          IsEnabled = h.IsEnabled,
+                                          Date = h.Date
+                                      });
+            return lastVIdeosWatched.ToList();
+        }
+
+        //POST api/history
+        [HttpPost]
+        public VideoWatchedDto AddToHistory(string videoId)
+        {
+            History video = new History();
+            video.VideoId = videoId;
+            video.IsEnabled = true;
+            video.Date = DateTime.Now;
+            _dbContext.History.Add(video);
+            _dbContext.SaveChanges();
+            var videoGuardado = _dbContext.History.Where(x => x.VideoId == videoId).Select(x => new VideoWatchedDto
+            {
+                Id = x.Id,
+                VideoId = x.VideoId,
+                IsEnabled = x.IsEnabled,
+                Date = x.Date
+            }).ToList();
+            var v = videoGuardado[0];
+            return v;
+        }
+
+        //DELETE api/history/{id}
+        [HttpDelete]
+        public void DeleteFromHistory(int id)
+        {
+            var itemToRemove = _dbContext.History.FirstOrDefault(h => h.Id == id);
+            if (itemToRemove != null)
+                itemToRemove.IsEnabled = false;
+            _dbContext.SaveChanges();
+        }
+    }
+}
---
+++ HistoryAPI/Models/VideoWatchedDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HistoryAPI.Models
+{
+    public class VideoWatchedDto
+    {
+        public int Id { get; set; }
+        public string VideoId { get; set; }
+        public bool IsEnabled { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
